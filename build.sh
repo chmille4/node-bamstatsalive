4a1c9b4dccf2162ec943eca78aa818c082e985c0
Chase Miller <chmille4@gmail.com>
2018-02-10 11:29:07 -0500

    Fix build script

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd bamstatsAlive
-BAMTOOLS=../node-bamtools/bamtools/ make
-cd ../../../.bin/
-ln -s ../node-bamstatsalive/bamstatsAlive bamstatsAlive
+BAMTOOLS=../../node-bamtools/bamtools/ make
+cd ../../.bin/
+ln -s ../../node-bamstatsalive/bamstatsAlive bamstatsAlive
