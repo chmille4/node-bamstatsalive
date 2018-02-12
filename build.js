78fcb93e5c6076079927d16f521f8fa29a5129a5
Chase Miller <chmille4@gmail.com>
2018-02-11 19:46:53 -0500

    Remove package

--- build.js
+++ build.js
@@ -4,8 +4,6 @@ const { spawn } = require('child_process');
 let pathToModule = require.resolve('node-bamtools')
 let path = require('path').dirname(pathToModule) + "/bamtools/"
 
-// let compile = spawn('make',[],{cwd: './bamstatsAlive/', env:{'BAMTOOLS':path}});
-
 let compile = spawn('./build.sh',[path]);
 
 compile.stdout.pipe(process.stdout);
